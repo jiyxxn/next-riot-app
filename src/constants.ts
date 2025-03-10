d2c9158bcf3aba4b63512077b9286108be2fff6b
jinew <jiyunk@naver.com>
2025-03-10 23:28:05 +0900

    ✨ Update : base url constants 정의

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
-export const ITEM_IMAGE_BASE_URL =
-  'https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/';
+export const RIOT_API_BASE_URL =
+  'https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR';
+export const RIOT_IMAGE_BASE_URL =
+  'https://ddragon.leagueoflegends.com/cdn/15.5.1/img';
+export const CHAMPION_SPLASH_IMAGE_BASE_URL =
+  'https://ddragon.leagueoflegends.com/cdn/img/champion/splash';
