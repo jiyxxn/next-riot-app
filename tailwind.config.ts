237358fda1e992fd825a1f8717ad4854390bcdf6
jinew <jiyunk@naver.com>
2025-03-10 14:42:44 +0900

    🎨 Styling : 글로벌 CSS 스타일링

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'yellow-dark': 'var(--color-yellow-dark)',
+        'yellow-pure': 'var(--color-yellow-pure)',
+        'yellow-light': 'var(--color-yellow-light)',
+        'white-dark': 'var(--color-white-dark)',
+        'white-pure': 'var(--color-white-pure)',
+        'white-light': 'var(--color-white-light)',
       },
     },
   },
