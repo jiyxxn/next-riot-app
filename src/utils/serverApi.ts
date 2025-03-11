6748d4741b7c7c289abc19f10115c901497bae44
jinew <jiyunk@naver.com>
2025-03-11 21:01:03 +0900

    🩹 Fix : error thow 방식으로 변경

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -2,65 +2,39 @@ import { BASE_URL } from '@/constants';
 import { Champion, ChampionDetail } from '@/types/Champion';
 import { Item } from '@/types/Items';
 
-export const fetchItemList = async (): Promise<Record<string, Item> | null> => {
-  try {
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+export const fetchItemList = async (): Promise<Record<string, Item>> => {
+  const response = await fetch(`${BASE_URL.RIOT_API}/item.json`);
 
-    const response = await fetch(`${BASE_URL.RIOT_API}/item.json`);
+  if (!response.ok) throw new Error(`HTTP 오류: ${response.status}`);
 
-    if (!response.ok) {
-      throw new Error(`HTTP 오류: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data.data;
-  } catch (error) {
-    console.error('아이템 리스트 fetch 에러', error);
-    return null;
-  }
+  const data = await response.json();
+  return data.data;
 };
 
-export const fetchChampionList = async (): Promise<Record<
-  string,
-  Champion
-> | null> => {
-  try {
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+export const fetchChampionList = async (): Promise<
+  Record<string, Champion>
+> => {
+  const response = await fetch(`${BASE_URL.RIOT_API}/champion.json`, {
+    next: { revalidate: 86400 },
+  });
 
-    const response = await fetch(`${BASE_URL.RIOT_API}/champion.json`, {
-      next: { revalidate: 86400 },
-    });
+  if (!response.ok) throw new Error(`HTTP 오류: ${response.status}`);
 
-    if (!response.ok) {
-      throw new Error(`HTTP 오류: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data.data;
-  } catch (error) {
-    console.error('챔피언 리스트 fetch 에러', error);
-    return null;
-  }
+  const data = await response.json();
+  return data.data;
 };
 
 export const fetchChampionDetail = async (
   id: string
-): Promise<Record<string, ChampionDetail> | null> => {
-  try {
-    const response = await fetch(`${BASE_URL.RIOT_API}/champion/${id}.json`, {
-      cache: 'no-store',
-    });
+): Promise<Record<string, ChampionDetail>> => {
+  const response = await fetch(`${BASE_URL.RIOT_API}/champion/${id}.json`, {
+    cache: 'no-store',
+  });
 
-    if (!response.ok) {
-      throw new Error(`HTTP 오류: ${response.status}`);
-    }
+  if (!response.ok) throw new Error(`HTTP 오류: ${response.status}`);
 
-    const data = await response.json();
-    return data.data;
-  } catch (error) {
-    console.error('챔피언 상세 fetch 에러', error);
-    return null;
-  }
+  const data = await response.json();
+  return data.data;
 };
 
 export const getRotationChampionList = async (): Promise<Champion[]> => {
