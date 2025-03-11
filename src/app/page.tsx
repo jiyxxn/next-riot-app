072b27e2b7ab6af06474cbbb427b6d406759dab3
jinew <jiyunk@naver.com>
2025-03-11 20:29:54 +0900

    🩹 Fix : 메인 레이아웃 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,47 +3,48 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <section className="text-white-pure min-h-[100vh] flex flex-col justify-center text-center">
-      <h2 className="font-serif text-5xl text-yellow-light mb-4">
-        League Of Legends Information
-      </h2>
-      <p>Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</p>
+    <section className="text-white-pure ">
+      <h2 className="sr-only">League Of Legends Information</h2>
 
-      <ul className="flex justify-between w-full px-20 gap-4 mt-20">
-        <li>
+      <ul className="flex w-full min-h-[100vh]">
+        <li className="relative flex-1 hover:flex-[3] transition-all duration-700 group">
           <Link href={'/champions'}>
             <Image
-              src="https://img.uhdpaper.com/wallpaper/divine-god-king-darius-fallen-god-king-garen-skin-lol-splash-art-355@2@a-preview.jpg?dl"
+              src="/kings.jpg"
               alt="챔피언 정보 보기"
-              width={400}
-              height={400}
-              className="rounded-sm"
+              fill
+              className="object-cover grayscale transition-all duration-300 group-hover:grayscale-0"
             />
-            <span className="block py-4">챔피언 정보 보기</span>
+            <span className="tracking-tight text-3xl font-bold absolute w-full text-center block py-4 bottom-20 left-1/2 -translate-x-1/2 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              챔피언 정보 보기
+            </span>
           </Link>
         </li>
-        <li>
+
+        <li className="relative flex-1 hover:flex-[3] transition-all duration-700 group">
           <Link href={'/rotation'}>
             <Image
-              src="https://img.uhdpaper.com/wallpaper/t1-2024-jayce-orianna-lee-sin-jinx-bard-skins-lol-splash-art-327@2@a-preview.jpg?dl"
+              src="/t1.jpg"
               alt="금주 로테이션 확인"
-              width={400}
-              height={400}
-              className="rounded-sm"
+              fill
+              className="object-cover grayscale transition-all duration-300 group-hover:grayscale-0"
             />
-            <span className="block mt-4">금주 로테이션 확인</span>
+            <span className="tracking-tight text-3xl font-bold absolute w-full text-center block py-4 bottom-20 left-1/2 -translate-x-1/2 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              금주 로테이션 확인
+            </span>
           </Link>
         </li>
-        <li>
+        <li className="relative flex-1 hover:flex-[3] transition-all duration-700 group">
           <Link href={'/items'}>
             <Image
-              src="https://img.uhdpaper.com/wallpaper/arcane-netflix-series-380@3@b-preview.jpg?dl"
+              src="/neeko.jpg"
               alt="아이템 정보 보기"
-              width={400}
-              height={400}
-              className="rounded-sm"
+              fill
+              className="object-cover grayscale transition-all duration-300 group-hover:grayscale-0"
             />
-            <span className="block mt-4">아이템 정보 보기</span>
+            <span className="tracking-tight text-3xl font-bold absolute w-full text-center block py-4 bottom-20 left-1/2 -translate-x-1/2 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              아이템 정보 보기
+            </span>
           </Link>
         </li>
       </ul>
