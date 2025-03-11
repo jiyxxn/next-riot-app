a7dcf752a5653b2bf6a49c9f13d737c0887d1821
jinew <jiyunk@naver.com>
2025-03-11 16:16:10 +0900

    🩹 Fix : const 타입 선언

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const RIOT_API_BASE_URL =
-  'https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR';
-export const RIOT_IMAGE_BASE_URL =
-  'https://ddragon.leagueoflegends.com/cdn/15.5.1/img';
-export const CHAMPION_SPLASH_IMAGE_BASE_URL =
-  'https://ddragon.leagueoflegends.com/cdn/img/champion/splash';
+export const BASE_URL = {
+  RIOT_API: 'https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR',
+  RIOT_IMAGE: 'https://ddragon.leagueoflegends.com/cdn/15.5.1/img',
+  CHAMPION_SPLASH_IMAGE:
+    'https://ddragon.leagueoflegends.com/cdn/img/champion/splash',
+} as const;
