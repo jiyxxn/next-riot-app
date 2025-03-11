431d39d51681ab4dd93e2e0c2713ffb794c07e08
jinew <jiyunk@naver.com>
2025-03-11 17:19:41 +0900

    ✨ Update : Constant URL 사용

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -2,19 +2,16 @@ import { BASE_URL } from '@/constants';
 import { Champion } from '@/types/Champion';
 
 export async function GET() {
-  const rotationResponse = await fetch(
-    'https://kr.api.riotgames.com/lol/platform/v3/champion-rotations',
-    {
-      headers: {
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Whale/4.30.291.11 Safari/537.36',
-        'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
-        Origin: 'https://developer.riotgames.com',
-        'X-Riot-Token': `${process.env.NEXT_PUBLIC_RIOT_API_KEY}`,
-      },
-    }
-  );
+  const rotationResponse = await fetch(BASE_URL.ROTATION_CHAMPIONS, {
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Whale/4.30.291.11 Safari/537.36',
+      'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+      'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
+      Origin: 'https://developer.riotgames.com',
+      'X-Riot-Token': `${process.env.NEXT_PUBLIC_RIOT_API_KEY}`,
+    },
+  });
 
   const championListResponse = await fetch(
     `${BASE_URL.RIOT_API}/champion.json`
--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,8 @@
 export const BASE_URL = {
   RIOT_API: 'https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR',
   RIOT_IMAGE: 'https://ddragon.leagueoflegends.com/cdn/15.5.1/img',
+  ROTATION_CHAMPIONS:
+    'https://kr.api.riotgames.com/lol/platform/v3/champion-rotations',
   CHAMPION_SPLASH_IMAGE:
     'https://ddragon.leagueoflegends.com/cdn/img/champion/splash',
 } as const;
