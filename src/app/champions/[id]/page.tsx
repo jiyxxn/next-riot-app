d689414ce89dc7a23242906b9ff8e704045cdb9f
jinew <jiyunk@naver.com>
2025-03-12 14:30:31 +0900

    ✨ Update : 스킬 툴팁 추가

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { BASE_URL } from '@/constants';
 import { ChampionDetail } from '@/types/Champion';
+import removeHtmlTags from '@/utils/removeHtmlTags';
 import { fetchChampionDetail } from '@/utils/serverApi';
 import Image from 'next/image';
 
@@ -10,19 +11,21 @@ const ChampionDetailPage = async ({ params }: { params: { id: string } }) => {
   const champion = championData[params.id];
 
   return (
-    <section className="pt-[80px]">
+    <section className="pt-[60px] lg:pt-[80px]">
       <div
-        className="relative w-full pt-48 px-20 min-h-[calc(100vh_-_80px)]"
+        className="relative w-full pb-20 pt-24 px-8 md:pt-48 md:px-20 min-h-[calc(100vh_-_60px)] lg:min-h-[calc(100vh_-_80px)] sm:!bg-cover md:!bg-center"
         style={{
           background: `
       linear-gradient(to right, rgba(0, 0, 0, 0.35) 0%, rgba(0, 0, 0, 0) 50%),
-      url('${BASE_URL.CHAMPION_SPLASH_IMAGE}/${champion.id}_0.jpg') no-repeat center center / cover
+      url('${BASE_URL.CHAMPION_SPLASH_IMAGE}/${champion.id}_0.jpg') no-repeat top center / contain
     `,
         }}>
         <div className="relative z-10 text-white mix-blend-color-dodge">
-          <h2 className="flex flex-col gap-2 text-yellow-light">
-            <span className="text-8xl font-serif font-bold">{champion.id}</span>
-            <span className="text-3xl font-bold ml-4">
+          <h2 className="flex flex-col gap-1 lg:gap-2 text-yellow-light">
+            <span className="text-5xl md:text-8xl font-serif font-bold">
+              {champion.id}
+            </span>
+            <span className="text-xl md:text-3xl font-bold ml-2 lg:ml-4">
               {champion.name}{' '}
               <span className="text-lg font-semibold ml-2 tracking-tight">
                 {champion.title}
@@ -30,13 +33,13 @@ const ChampionDetailPage = async ({ params }: { params: { id: string } }) => {
             </span>
           </h2>
         </div>
-        <div className="w-1/3 px-4 py-6">
+        <div className="lg:w-1/3 px-4 py-6 mt-16 lg:mt-0">
           <p className="drop-shadow-[0_2px_4px_rgba(0,0,0,0.4)] text-white-light ">
             {champion.lore}
           </p>
 
-          <ul className="flex gap-4 mt-10">
-            <li>
+          <ul className="relative flex gap-4 mt-10 mb-20">
+            <li className="group cursor-pointer">
               <Image
                 src={`${BASE_URL.RIOT_IMAGE}/passive/${champion.passive.image.full}`}
                 width={60}
@@ -44,9 +47,12 @@ const ChampionDetailPage = async ({ params }: { params: { id: string } }) => {
                 alt={champion.passive.name}
                 className="rounded-sm"
               />
+              <p className="group-hover:block hidden absolute left-0 top-full mt-2 w-full text-yellow-pure text-sm bg-zinc-950 bg-opacity-80 p-4 rounded-md">
+                {removeHtmlTags(champion.passive.description)}
+              </p>
             </li>
             {champion.spells.map((spell) => (
-              <li key={spell.id}>
+              <li key={spell.id} className="group cursor-pointer">
                 <Image
                   src={`${BASE_URL.RIOT_IMAGE}/spell/${spell.image.full}`}
                   width={60}
@@ -54,6 +60,9 @@ const ChampionDetailPage = async ({ params }: { params: { id: string } }) => {
                   alt={spell.name}
                   className="rounded-sm"
                 />
+                <p className="group-hover:block hidden absolute left-0 top-full mt-2 w-full text-yellow-pure text-sm bg-zinc-950 bg-opacity-80 p-4 rounded-md">
+                  {removeHtmlTags(spell.description)}
+                </p>
               </li>
             ))}
           </ul>
