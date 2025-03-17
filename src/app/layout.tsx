bb0d9686439242b8b21ba9ca17022d7a25d4ac84
jinew <jiyunk@naver.com>
2025-03-17 16:36:20 +0900

    🩹 fix : 스타일링 파일 반영

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import './globals.css';
+import '../styles/globals.css';
 import { notoSans } from '@/styles/fonts/Noto-Sans';
 import Header from '@/components/Header';
 import Providers from './providers';
--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,3 +1,5 @@
+import '../styles/loader.css';
+
 const LoadingSpinner = () => {
   return <span className="loader"></span>;
 };
